1279d25131cf093aa76a1c552174ccc40d7b9744
keito <43479281+keito4701@users.noreply.github.com>
2018-11-17 21:39:30 -0600

    hehexd

---
+++ Assets/Scripts/Textures/ChangeColorBackground.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorBackground : MonoBehaviour {
+
+    SpriteRenderer m_SpriteRenderer;            //The Color to be assigned to the Renderer’s Material
+    Color m_NewColor;
+
+    //These are the values that the Color Sliders return
+    float m_Red, m_Blue, m_Green;
+
+
+    // Use this for initialization
+    void Start () {
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();          //get SpriteRenderer
+
+        m_Green = 1;
+        m_Red = 1;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float y = transform.position.y / 200 + 1;
+
+        m_NewColor = new Color(y, y, y);
+
+        //Set the SpriteRenderer to the Color defined by the Sliders
+        m_SpriteRenderer.color = m_NewColor;
+    }
+}
